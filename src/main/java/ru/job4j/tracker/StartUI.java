130ad09d3522fcd91cb0274a34fdfd5e45f20b22
fromiva <66008363+fromiva@users.noreply.github.com>
2022-03-13 21:04:46 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        System.out.println(item.getCreated().format(ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+    }
+}
