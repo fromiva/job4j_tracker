8a6ef7afce897050070a4ff2efa3543ac1d4d87a
fromiva <66008363+fromiva@users.noreply.github.com>
2022-03-19 15:03:29 +0300

    Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,27 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[items.length];
+        int size = 0;
+        for (Item item : items) {
+            if (item != null && key.equals(item.getName())) {
+                result[size++] = item;
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[items.length];
+        int size = 0;
+        for (Item item : items) {
+            if (item != null) {
+                result[size++] = item;
+            }
+        }
+        return Arrays.copyOf(result, size);
     }
 }
