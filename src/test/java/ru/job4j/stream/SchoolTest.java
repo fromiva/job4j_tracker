34efeae45034d11bf39b50962242a41da41f429f
fromiva <66008363+fromiva@users.noreply.github.com>
2022-05-17 13:34:34 +0300

    1. Фильтрация учеников [#110058 #301528]. Исправление теста

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -17,7 +17,7 @@ public class SchoolTest {
                 new Student(90, "Surname9")
         );
         School school = new School();
-        Predicate<Student> predicate = student -> student.getScore() > 50;
+        Predicate<Student> predicate = student -> student.getScore() >= 70;
         List<Student> expected = List.of(
                 new Student(70, "Surname7"),
                 new Student(90, "Surname9")
